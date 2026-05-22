9095576a2aea04da6a8363fd54375fdcfcfc0068
soleros <soleromel@gmail.com>
2026-05-22 07:46:09 +0200

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/troglo/',
+  base: '/',
   plugins: [tailwindcss(), react()],
 })
